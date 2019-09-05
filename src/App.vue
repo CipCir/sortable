8681f2a79ef5b82c0f40e1ad0a27d05fb1210507
cip <cip>
2019-09-05 11:01:12 +0300

    cip

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,92 @@
 <template>
   <div id="app">
     <div>Please rank the below statements in your order of preference:</div>
-  <br>
-  <div class="answerContainer">
-
-    <div class="answerRow" v-for="(answer,indx) in answers" :key="indx">
-      <span @click="moveAnsw(false,indx,answer)" class="srtBtn left">	&#8593;</span>
-      <span class="answer">{{answer}}</span>
-      <span @click="moveAnsw(true,indx,answer)" class="srtBtn right">&#8595;	</span>
+    <br />
+    <div class="answerContainer">
+      <div class="answerRow" v-for="(answer,indx) in answers" :key="indx">
+        <span @click="moveAnsw(false,indx,answer.lbl)" class="srtBtn left">&#8593;</span>
+        <span class="answer">{{answer.lbl}}</span>
+        <span @click="moveAnsw(true,indx,answer.lbl)" class="srtBtn right">&#8595;</span>
+      </div>
     </div>
-  </div>
-
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'app',
- data(){
-   return{
-     answers:["First answer","Second answer","Third answer","Forth answer"]
-   }
- },
- methods:{
-   moveAnsw(UP,indx,currVal){     
-     let origVal=""
-     let newIndx
-     if (UP){
-       newIndx=indx+1
-       if (newIndx<this.answers.length){
-         origVal=this.answers[newIndx]
-         this.$set(this.answers,newIndx,currVal)
-         this.$set(this.answers,indx,origVal)
-         
-       }else{
-         return false
-       }
-
-     }else{
-         newIndx=indx-1
-       if (newIndx>=0){
-         origVal=this.answers[newIndx]
-         this.$set(this.answers,newIndx,currVal)
-         this.$set(this.answers,indx,origVal)
-         
-       }else{
-         return false
-       }
-
-     }
-   }
- }
-}
+  name: "app",
+  data() {
+    return {
+      answers: []
+    };
+  },
+  created() {
+    var vueObj = this;
+    $(".mrGridCategoryText").each(function(indx) {
+      vueObj.answers.push({ lbl: $(this).text(), id: indx });
+    });
+  },
+  methods: {
+    moveAnsw(UP, indx, currVal) {
+      let origVal = "";
+      let newIndx;
+      if (UP) {
+        newIndx = indx + 1;
+        if (newIndx < this.answers.length) {
+          origVal = this.answers[newIndx];
+          this.$set(this.answers, newIndx, currVal);
+          this.$set(this.answers, indx, origVal);
+        } else {
+          return false;
+        }
+      } else {
+        newIndx = indx - 1;
+        if (newIndx >= 0) {
+          origVal = this.answers[newIndx];
+          this.$set(this.answers, newIndx, currVal);
+          this.$set(this.answers, indx, origVal);
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+};
 </script>
 
-<style>
-.answerRow{
-  margin:5px 0
+<style scoped>
+.answerRow {
+  margin: 5px 0;
 }
-.answerContainer{
-      width: 50%;
-    margin: 0 auto;
-    text-align: left;
+.answerContainer {
+  width: 50%;
+  margin: 0 auto;
+  text-align: left;
 }
 @media only screen and (max-width: 600px) {
   .answerContainer {
     width: 100%;
   }
 }
-.left{
-  float:left;
+.left {
+  float: left;
 }
-.right{
-  float:right
+.right {
+  float: right;
 }
-.srtBtn{
+.srtBtn {
   cursor: pointer;
   background: lightgrey;
-  margin:0 10px;
-      width: 30px;
-    text-align: center;
+  margin: 0 10px;
+  width: 30px;
+  text-align: center;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
-
 </style>
